3873f3a923b4629316599cca8feb541d90c035cd
Daniel Hazelton <dshadowwolf@gmail.com>
2019-04-14 14:02:23 -0400

    fix the client-on-server issue, update the changelog

--- src/main/java/com/mcmoddev/lib/vanillabits/VanillaBitsRegistryEvents.java
+++ src/main/java/com/mcmoddev/lib/vanillabits/VanillaBitsRegistryEvents.java
@@ -21,6 +21,8 @@ import net.minecraftforge.client.model.ModelLoader;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
 
 @Mod.EventBusSubscriber(modid=MMDLib.MODID)
 public class VanillaBitsRegistryEvents {
@@ -51,6 +53,7 @@ public class VanillaBitsRegistryEvents {
 		Oredicts.registerItemOreDictionaryEntries();
 	}
 	
+	@SideOnly(Side.CLIENT)
 	@SubscribeEvent
 	public static void modelRegistryBits(ModelRegistryEvent ev) {
 		Arrays.asList(VanillaMaterialNames.CHARCOAL, VanillaMaterialNames.COAL,
